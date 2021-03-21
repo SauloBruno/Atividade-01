814fd2c0bd2f011f98569d878a67354b23ab8127
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-03-21 17:41:44 -0300

    Primeiro Commit

---
+++ Atividade1/Controllers/ExternoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atividade1.Controllers
+{
+    public class ExternoController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
