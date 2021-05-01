0901135bd61bd74b5a8214f997d7936c54945be0
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-04-30 21:46:32 -0300

    Questão 1 APS

---
+++ Atividade1/Models/Cliente/ClienteEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Atividade1.Models.Cliente
+{
+    public class ClienteEntity
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ Atividade1/Models/EventoEntity/EventoEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Atividade1.Models.EventoEntity
+{
+    public class EventoEntity
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
