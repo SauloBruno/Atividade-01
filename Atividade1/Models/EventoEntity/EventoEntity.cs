ab221d4caf06af8916df32e2f8cdd7b3b39fd219
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-04-30 21:56:06 -0300

    Questão 4 APS

--- Atividade1/Models/EventoEntity/EventoEntity.cs
+++ Atividade1/Models/EventoEntity/EventoEntity.cs
@@ -1,9 +1,36 @@
-﻿namespace Atividade1.Models.EventoEntity
+﻿using System;
+using Atividade1.Models.Cliente;
+using Atividade1.Models.Situacao;
+
+namespace Atividade1.Models.EventoEntity
 {
     public class EventoEntity
     {
         public int Id { get; set; }
+        
         public string Descricao { get; set; }
+        
+        public string Tipo { get; set; }
 
+        public DateTime DataHoraInicio { get; set; }
+
+        public DateTime DataHoraTermino { get; set; }
+
+        public ClienteEntity Cliente { get; set; }
+
+        public SituacaoEventoEntity Situacao { get; set; }
+
+        public string Local { get; set; }
+
+        public string TextoObservacao { get; set; }
+
+        public DateTime DataInsercao { get; set; }
+
+        public DateTime DataUltimaModificacao { get; set; }
+        public EventoEntity(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
     }
 }
