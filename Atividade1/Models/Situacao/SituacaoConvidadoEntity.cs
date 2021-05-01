d43c0eb5a3959d2b3c3c6872cc1441074139c751
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-04-30 21:49:26 -0300

    Questão 2 APS

---
+++ Atividade1/Models/Situacao/SituacaoConvidadoEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Atividade1.Models.Situacao
+{
+    public class SituacaoConvidadoEntity
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
